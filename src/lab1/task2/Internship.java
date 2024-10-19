e85c57fda4435960215303d9fa384541df6b3320
Cristi_Ciopa <36815812+Cristinelul345@users.noreply.github.com>
2024-10-19 22:57:35 +0300

    Add files via upload
    Added lab01

---
+++ src/lab1/task2/Internship.java
@@ -0,0 +1,61 @@
+package lab1.task2;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Internship {
+    private String name;
+    private String minGrade;
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public void setStudents(Student[] students) {
+        this.students = students;
+    }
+
+    public String getMinGrade() {
+        return minGrade;
+    }
+
+    public void setMinGrade(String minGrade) {
+        this.minGrade = minGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private Student[] students = new Student[3];
+    Random random = new Random();
+
+
+    public Internship(String name, String minGrade) {
+        this.name = name;
+        this.minGrade = minGrade;
+    }
+
+
+    public Student chooseCandidateRandomly(Student[] listStuds) {
+        // Generate a random index
+        int randomIndex = random.nextInt(listStuds.length);
+
+        // Return the student at the random index
+        return listStuds[randomIndex];
+    }
+
+    public Internship chooseCandidatesForInterview(Student[] listStuds, Internship internship) {
+        for (int i = 0; i < listStuds.length; i++) {
+            if( listStuds[i].getGrade() >= Double.parseDouble(internship.getMinGrade()) ) {
+                System.out.println("Candidate: " +  listStuds[i].getName() + " got a phone interview at " + internship.getName());
+            }
+        }
+        return null;
+    }
+}
---
+++ src/lab1/task2/JobMarket.java
@@ -0,0 +1,42 @@
+package lab1.task2;
+import java.util.ArrayList;
+
+public class JobMarket {
+    public static void main(String[] args) {
+        Student stud0 = new Student("Gigel", 5.7);
+        Student stud1 = new Student("Dorel", 7.7);
+        Student stud2 = new Student("Marcel", 9.7);
+        Student stud3 = new Student("Ionel", 4.0);
+
+        // equal case
+        Student stud4 = new Student("James", 5.7);
+        Student stud5 = new Student("James", 5.7);
+        if (stud4.equals(stud5)) {
+            System.out.println("Student is the same");
+        }
+
+        Student[] students = new Student[]{stud0, stud1, stud2, stud3};
+        //more changes need to be done in Internshi.java to make arraylis work
+//        ArrayList<Student> students = new ArrayList<>();
+//        students.add(stud0);
+//        students.add(stud1);
+//        students.add(stud2);
+//        students.add(stud3);
+
+        Internship company1 = new Internship("Google", "8");
+        Internship company2 = new Internship("Facebook", "9");
+        Internship company3 = new Internship("Amazon", "7");
+        Internship company4 = new Internship("Microsoft", "6");
+
+
+        // Call the chooseCandidateRandomly method and print the result
+        Student randomStudent = company1.chooseCandidateRandomly(students);
+
+        System.out.println("Randomly selected student: " + randomStudent.getName());
+
+        company1.chooseCandidatesForInterview(students, company1);
+        company2.chooseCandidatesForInterview(students, company2);
+        company3.chooseCandidatesForInterview(students, company3);
+        company4.chooseCandidatesForInterview(students, company4);
+    }
+}
